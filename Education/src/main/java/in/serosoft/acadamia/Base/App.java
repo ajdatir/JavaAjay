88a8426a31ad766b1783043a09cb556a53c0aba0
ajdatir <ajaydatir77@gmail.com>
2024-02-09 00:40:55 +0530

    commitby now

---
+++ Education/src/main/java/in/serosoft/acadamia/Base/App.java
@@ -0,0 +1,13 @@
+package in.serosoft.acadamia.Base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Pala/src/com/demo/Pallendrom.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Pallendrom {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome");
+		
+		
+	}
+
+}
