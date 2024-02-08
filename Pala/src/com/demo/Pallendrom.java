6c2a09bebc7f40675212c87af8d0c019de7f52ad
ajdatir <ajaydatir77@gmail.com>
2024-02-09 00:58:53 +0530

    gggggggggggggg

--- Pala/src/com/demo/Pallendrom.java
+++ Pala/src/com/demo/Pallendrom.java
@@ -5,8 +5,7 @@ public class Pallendrom {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome");
-		System.out.println("Welcome");
-		System.out.println("Welcome");
+		
 		
 	}
 
